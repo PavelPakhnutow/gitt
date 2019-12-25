97c43d820e2753c9045033ec6ca3872cb97a081c
PavelPakhnutow <pavelpakhnutow@gmail.com>
2019-12-26 01:20:16 +0300

    Start

---
+++ pr/git_task_2_prj.cpp
@@ -0,0 +1,51 @@
+﻿// git_task_2_prj.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int n; 
+
+	do
+	{
+		
+
+		switch (n)
+		{
+		case 1: 
+		{
+			system("pause");
+			break;
+		}
+
+		case 2: 
+		{
+			system("pause");
+			break;
+		}
+		case 3: 
+		{
+			system("pause");
+			break;
+		}
+		case 4:
+		{
+			system("pause");
+			break;
+		}
+		case 0: 
+		{
+			break;
+		}
+		default: 
+		{
+			system("pause");
+		}
+		}
+	} while (n != 0); 
+
+	return 0;
+}
