610c02ada79645222268a5c86d8b912edf986356
Daniil Petrov <sadness18@icloud.com>
2019-12-26 01:47:16 +0300

    add_sum

---
+++ pr/class.cpp
@@ -0,0 +1,16 @@
+#include "class.h"
+#include <iostream>
+using namespace std;
+
+void funcs::sum()
+{
+	cout << "Input first number: ";
+	cin >> first;
+	cout << "Input second number: ";
+	cin >> second;
+	system("cls");
+	result = first + second;
+	cout << "Your result: " << first << " + " << second << " = " << result << endl;
+	system("pause");
+	system("cls");
+}
---
+++ pr/class.h
@@ -0,0 +1,12 @@
+#ifndef _CLASS_H_
+#define _CLASS_H_
+
+class funcs
+{
+private:
+	float first, second, result;
+public:
+	void sum();
+};
+
+#endif // !_CLASS_H_
